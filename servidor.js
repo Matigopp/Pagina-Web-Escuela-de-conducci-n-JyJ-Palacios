3d0881d178c581b32316d178e2da060e3d73c453
matigop <matigreig@gmail.com>
2026-01-23 19:12:35 -0300

    supuesto fix

--- servidor.js
+++ servidor.js
@@ -1,6 +1,13 @@
 const path = require('path');
 const express = require('express');
 const { obtenerPool, probarConexion, hayConfiguracionBaseDatos } = require('./configuracion/conexionBaseDatos');
+// Sirve todos los archivos estáticos desde la raíz del proyecto
+app.use(express.static(__dirname));
+
+// Home
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, "index.html"));
+});
 
 const app = express();
 const puerto = Number(process.env.PUERTO_APP) || 3000;
